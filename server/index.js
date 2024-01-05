96740bfcad60ff9f6662f923d640f93da14d0601
eropka11 <tujh1993@mail.ru>
2024-01-05 13:58:44 +0300

    dabase models and dependenses added

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import envConfig from 'dotenv';
 import sequelize from './db.js';
 
@@ -7,6 +8,8 @@ envConfig.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const start = async () => {
   try {
--- server/models/models.js
+++ server/models/models.js
@@ -5,23 +5,82 @@ const User = sequelize.define('user', {
   id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
   password: {type: DataTypes.INTEGER},
   profile: {type: DataTypes.JSON},
-  roles: {type: DataTypes.STRING, defaultValue: 'USER'},
-  subscriptions: {type: DataTypes.ARRAY},
+  role: {type: DataTypes.STRING, defaultValue: 'USER'},
+  subscriptions: {type: DataTypes.ARRAY(DataTypes.INTEGER)},
   avatar: {type: DataTypes.STRING},
 });
 
 const Diary = sequelize.define('dairy', {
   id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-  diaryName: {type: DataTypes.STRING},
-  subscribers: {type: DataTypes.ARRAY},
+  diaryName: {type: DataTypes.STRING, allowNull: false},
+  subscribers: {type: DataTypes.ARRAY(DataTypes.INTEGER)},
 });
 
 const Entry = sequelize.define('entry', {
   id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-  title: {type: DataTypes.INTEGER},
-  text: {type: DataTypes.INTEGER},
-  likes: {type: DataTypes.INTEGER},
-  img: {type: DataTypes.ARRAY},
-  video: {type: DataTypes.ARRAY},
-  audio: {type: DataTypes.ARRAY},
+  title: {type: DataTypes.STRING, allowNull: false},
+  text: {type: DataTypes.STRING, allowNull: false},
+  likes: {type: DataTypes.INTEGER, defaultValue: 0},
+  img: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false},
+  video: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false},
+  audio: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false},
 });
+
+const Comment = sequelize.define('comment', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+  text: {type: DataTypes.STRING},
+  likes: {type: DataTypes.INTEGER, defaultValue: 0},
+  img: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false},
+  video: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false},
+  audio: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false},
+});
+
+const Subscribe = sequelize.define('subscribe', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+const EntryLike = sequelize.define('entry_like', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+const CommentLike = sequelize.define('comment_like', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+
+User.hasMany(Diary);
+Diary.belongsTo(User);
+
+User.hasOne(Subscribe);
+Subscribe.belongsTo(User);
+
+User.hasOne(EntryLike);
+EntryLike.belongsTo(User);
+
+User.hasOne(CommentLike);
+CommentLike.belongsTo(User);
+
+Diary.hasMany(Entry);
+Entry.belongsTo(Diary);
+
+Diary.hasMany(Subscribe);
+Subscribe.belongsTo(Diary);
+
+Entry.hasMany(Comment);
+Comment.belongsTo(Entry);
+
+Entry.hasMany(EntryLike);
+EntryLike.belongsTo(Entry);
+
+Comment.hasMany(CommentLike);
+CommentLike.belongsTo(Comment);
+
+export {
+  User,
+  Diary,
+  Entry,
+  Comment,
+  Subscribe,
+  EntryLike,
+  CommentLike,
+};
